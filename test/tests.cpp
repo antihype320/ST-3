c5c70cd318fbaca3fa8377e9a12412f6267df63d
Nikita <nikita200604prochorov@mail.ru>
2025-05-15 02:25:46 +0300

    fix-2

--- test/tests.cpp
+++ test/tests.cpp
@@ -11,12 +11,12 @@
 #include "TimedDoor.h"
 
 class MockThis : public TimerClient {
-public:
+ public:
   MOCK_METHOD(void, Timeout, (), (override));
 };
 
 class TimedDoorTest : public ::testing::Test {
-protected:
+ protected:
   TimedDoor door{ 20 };
   Timer timer;
   std::unique_ptr<MockThis> mockClient;
